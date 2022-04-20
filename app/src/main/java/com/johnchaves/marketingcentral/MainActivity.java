9d2f693632f696c06c95a5b24d782f4ca9e9b7a0
John C <jjchavescc@gmail.com>
2022-04-20 13:08:50 -0300

    - fix on elementos con warnings

--- app/src/main/java/com/johnchaves/marketingcentral/MainActivity.java
+++ app/src/main/java/com/johnchaves/marketingcentral/MainActivity.java
@@ -6,19 +6,16 @@ import android.content.Intent;
 import android.content.IntentSender;
 import android.os.Bundle;
 import android.os.StrictMode;
-import android.view.View;
 import android.widget.Button;
 import android.widget.EditText;
 import android.widget.TextView;
 import android.widget.Toast;
 
-import com.google.android.material.snackbar.Snackbar;
 import com.google.android.play.core.appupdate.AppUpdateInfo;
 import com.google.android.play.core.appupdate.AppUpdateManager;
 import com.google.android.play.core.appupdate.AppUpdateManagerFactory;
 import com.google.android.play.core.install.InstallStateUpdatedListener;
 import com.google.android.play.core.install.model.AppUpdateType;
-import com.google.android.play.core.install.model.InstallStatus;
 import com.google.android.play.core.install.model.UpdateAvailability;
 import com.google.android.play.core.tasks.Task;
 
